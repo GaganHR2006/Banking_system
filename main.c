ca2409c55a225ce956fe76e92024659b2145c036
Gagan H R <gagangagan3352nothing@gmail.com>
2025-08-25 16:37:11 +0530

    added main

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int menu();
+int menu(){
+    printf("1.Open New Account \n");
+    printf("2.List of Accounts \n");
+    printf("3.See Individual Account \n");
+    printf("4.Daily transaction \n");
+    printf("5.Edit account \n");
+}
+int main(){
+    menu();
+}
